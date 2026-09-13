5f2bf1105f5a8fecb31cfeca3bc8359ac5f3b4c7
Nguyen Long <61386176+long-nguyen12@users.noreply.github.com>
2026-09-13 21:35:23 +0200

    Update __init__.py

--- stages/__init__.py
+++ stages/__init__.py
@@ -4,7 +4,7 @@ MODELS = {
     "clip": "openai/clip-vit-base-patch32",
     # "vision": "HuggingFaceTB/SmolVLM2-500M-Video-Instruct",
     "vision": "Qwen/Qwen3-VL-2B-Instruct",
-    "text": "Qwen/Qwen3-1.7B",
+    "text": "Qwen/Qwen3-4B",
     "location": "dslim/bert-base-NER",
 }
 
